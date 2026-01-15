e835a9a3566b021456b57af26d6016b5458d8a1d
Testimony <gbadegesintestimony@gmail.com>
2026-01-15 09:52:12 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+this project is for the tutorial into Backend in Javascript
---
+++ backend/src/config/database.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const connectioInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}`
+    );
+
+    console.log(` 
+      \n MongoDB connected !!!
+      ${connectioInstance.connection.host}`);
+  } catch (error) {
+    console.log("MongoDB connection failed", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
